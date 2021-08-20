785f96c21ee0aed84cc16f4b39b0d39f7604038e
Garrett Yien <yien.garrett94@gmail.com>
2021-08-20 16:51:10 -0400

    built logout component

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axiosWithAuth from '../helpers/axiosWithAuth';
 
 const Logout = (props)=>{
-
+    useEffect(()=>{
+        axiosWithAuth()
+        .post('/logout')
+        .then(res=>{
+            localStorage.removeItem("token");
+            props.history.push('/login');
+        });
+    }, []);
+    return(
+        <div>You are logged out.</div>
+    );
 };
 
 export default Logout;
